70ffec0de223fa50d0c382277199b79f468fd6eb
cepiseptiyana <cepifams3@gmail.com>
2024-01-28 22:46:05 +0700

    first commit uye

---
+++ script.js
@@ -0,0 +1,8 @@
+// BUAT TRANSITION PADA NAVBAR [P]
+// PR =>
+
+let drop = document.querySelector(".kontak .box");
+let i = document.querySelector(".kontak p");
+i.addEventListener("click", function () {
+  drop.classList.toggle("toggle");
+});
