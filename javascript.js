a545441ecd6e7d0e404667addc39c198bb975984
cepiseptiyana <cepifams3@gmail.com>
2024-02-02 01:04:32 +0700

    six commit

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-// BUAT TRANSITION PADA NAVBAR [P]
-// PR =>
-
+// ANIMASI PADA "LOGO" NAVBAR
+let logo = document.querySelector(".logo");
+let animasiLogoNavbar = [...logo.textContent]
+  .map((el) => `<span>${el}</span>`)
+  .join("");
+logo.innerHTML = animasiLogoNavbar;
+// ANIMASI TAG "P" PADA NAVBAR
 let drop = document.querySelector(".kontak .box");
 let i = document.querySelector(".kontak p");
 i.addEventListener("click", function () {
