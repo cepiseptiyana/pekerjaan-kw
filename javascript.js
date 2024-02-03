0ab610bc072647fa2b346905e821581f2f072320
cepiseptiyana <cepifams3@gmail.com>
2024-02-03 16:47:23 +0700

    nine commit

--- javascript.js
+++ javascript.js
@@ -4,6 +4,21 @@ let animasiLogoNavbar = [...logo.textContent]
   .map((el) => `<span>${el}</span>`)
   .join("");
 logo.innerHTML = animasiLogoNavbar;
+
+// ANIMASI JUMBOTRON KANAN
+// IMAGE GALERY
+let leftFoto = document.getElementsByClassName("leftt")[0];
+let switchFoto = document.querySelector(".kanan-tengah .container");
+switchFoto.addEventListener("click", function (event) {
+  if (event.target.className == "image") {
+    leftFoto.src = event.target.src;
+    leftFoto.classList.add("fade");
+    setTimeout(function () {
+      leftFoto.classList.remove("fade");
+    }, 2000);
+  }
+});
+
 // ANIMASI TAG "P" PADA NAVBAR
 let drop = document.querySelector(".kontak .box");
 let i = document.querySelector(".kontak p");
